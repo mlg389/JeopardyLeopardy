90096d07a11334dbd4330ce0a9502acef787a611
mlg389 <mlg389@nau.edu>
2020-04-30 21:38:26 -0700

    Updated the main question page files to add "topic" to the custom widget

--- lib/gameTileWidget.dart
+++ lib/gameTileWidget.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 import 'answersPage.dart';
 
 class GameTileWidget extends StatelessWidget {
-  GameTileWidget({this.price});
+  GameTileWidget({this.price, this.topicTitle});
   final int price;
+  final String topicTitle;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => Navigator.push(
-          context, MaterialPageRoute(builder: (context) => AnswersPage())),
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  AnswersPage(difficulty: price, topic: topicTitle))),
       child: Card(
         child: Padding(
           padding:
