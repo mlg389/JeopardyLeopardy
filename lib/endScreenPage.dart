3e81f6c9e03cc25edae14f855d65485986ea8def
Aaron <pag87@nau.edu>
2020-05-01 21:01:01 -0700

    cleans up end game screen

--- lib/endScreenPage.dart
+++ lib/endScreenPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'main.dart';
+import 'TopicsWidget.dart';
 
 class EndScreen extends StatelessWidget {
   EndScreen({Key key, this.score}) : super(key: key);
@@ -8,29 +9,29 @@ class EndScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Text("Final Score:\n" + score.toString()),
-            ),
-            GestureDetector(
-              onTap: () => Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => MyApp())),
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Image.asset(
-                    'images/play.png',
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TopicsWidget(
+                  topic: 'Final Score: ' + score.toString(), fontSize: 50),
+              GestureDetector(
+                onTap: () => Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => MyApp())),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Image.asset(
+                      'images/play.png',
+                    ),
                   ),
+                  elevation: 5,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.yellow,
                 ),
-                elevation: 5,
-                margin: EdgeInsets.all(10),
-                color: Colors.yellow,
               ),
-            ),
-          ]),
+            ]),
+      ),
       backgroundColor: Colors.blueAccent,
     );
   }
